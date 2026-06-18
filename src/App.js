5757912bb10ff0c49619a5a661742ee035dc5670
Sasha <78424282+DeeDuh@users.noreply.github.com>
2026-06-19 00:50:34 +0300

    Creating a basket
    Deleting from a department, calculating the total amount of goods.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import Items from "./components/Items";
 class App extends React.Component {
   constructor(props){
     super(props)
-    this.state ={
+    this.state = {
+      orders: [],
       items: [
         {
           id: 1,
@@ -55,15 +56,31 @@ class App extends React.Component {
         },
       ]
     }
+    this.addToOrder = this.addToOrder.bind(this)
+    this.deleteOrder = this.deleteOrder.bind(this)
   }
   render(){
   return (
   <div className="wrapper">
-    <Header />
-    <Items items={this.state.items}/>
+    <Header orders={this.state.orders} onDelete={this.deleteOrder}/>
+    <Items items={this.state.items} onAdd={this.addToOrder}/>
     <Footer />
   </div>
   );
 }
+//функция удаления заказа:
+  deleteOrder(id){
+    this.setState({orders: this.state.orders.filter(el => el.id !== id)})
+  }  
+
+  addToOrder(item){
+    let isInArray = false
+    this.state.orders.forEach(el => {
+      if(el.id === item.id)
+        isInArray = true
+    })
+    if(!isInArray)
+      this.setState({orders: [...this.state.orders, item]})
+  }
 }
 export default App;
